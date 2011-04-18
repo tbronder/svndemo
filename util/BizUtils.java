fd287d77c6de1b9db7ce1492742e74e7cb1e7347
tyler.bronder <tyler.bronder@opower.com>
2011-04-18 15:56:36 -0400

    Updating the BizUtils class:  - created private constructor  - added method to create a FooBar

--- util/BizUtils.java
+++ util/BizUtils.java
@@ -1,5 +1,25 @@
 package tb.svndemo.util
 
+import tb.svndemo.Bar;
+import tb.svndemo.Foo;
+
 public class BizUtils {
+    /**
+     * A private constructor to prevent instantiation.
+     */
+    private BizUtils {}
+
     public static boolean getTrue() { return true; }
+
+    /**
+     * The Foo and Bar classes are linked to each other,
+     * so this util method will create a new instance of each,
+     * link them together, and then return the Foo.
+     */
+    public static Foo createFooBar() {
+        Bar bar = new Bar(new Foo());
+        Foo foo = bar.getFoo();
+        foo.setBar(bar);
+        return foo;
+    }
 }
