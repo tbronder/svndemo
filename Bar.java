c304fed15815ef8036e8dcc4cfe1355352d0d62f
tyler.bronder <tyler.bronder@opower.com>
2011-04-18 16:03:37 -0400

    Adding null checks to Foo and Bar class so the server doesn't catch fire.

--- Bar.java
+++ Bar.java
@@ -4,6 +4,10 @@ public class Bar {
     private Foo foo;
 
     public Bar(Foo foo) {
+        if (foo == null) {
+            throw new IllegalArgumentException("Foo must not be null.");
+        }
+
         this.foo = foo;
     }
 
